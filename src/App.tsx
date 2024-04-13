0252165d535e327709640e3b211a0fdf410fb01b
BirenKothari <birenkothari01@gmail.com>
2024-04-13 18:51:05 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="m-1 border border-s-red-100">
+      <h1 className="my-auto text-3xl font-bold underline">
+        To Do List
+      </h1>
+    </div>
+  )
+}
